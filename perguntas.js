312eecaaa08e144d60beba11194bc7ea1bf35d77
Piresbianca <140712531+Piresbianca@users.noreply.github.com>
2024-10-07 12:15:47 -0300

    bian

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,21 @@ criaCartao(
 'Como se diz tchau em inglẽs?',
 'Tchau em ingles é BYE (bai)' 
 )
+criaCartao (
+'Matemática',
+'Quanto é 10x10?',
+'é 100'
+)
+
+
+criaCartao(
+'Geogrfia',
+'Qual é a capital do Brasil?',
+'é Brasiĺia!'
+)
+
+criaCartao(
+'Artes',
+'Em que ano nasceu a Tarsila Amaral?',
+'Nasceu em 1 de setembro de 1886',
+)
